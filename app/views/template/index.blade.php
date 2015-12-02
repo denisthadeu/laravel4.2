41197fc8b2647a11ec53ff8997a00794a21d4a54
denisthadeu@hotmail.com <denisthadeu@hotmail.com>
2015-12-01 22:56:40 -0200

    Traduzi o texto de logout.

--- app/views/template/index.blade.php
+++ app/views/template/index.blade.php
@@ -182,13 +182,12 @@
                 <div class="mb-middle">
                     <div class="mb-title"><span class="fa fa-sign-out"></span> Log <strong>Out</strong> ?</div>
                     <div class="mb-content">
-                        <p>Are you sure you want to log out?</p>                    
-                        <p>Press No if youwant to continue work. Press Yes to logout current user.</p>
+                        <p>Tem certeza que você deseja deslogar do sistema?</p>                    
                     </div>
                     <div class="mb-footer">
                         <div class="pull-right">
-                            <a href="pages-login.html" class="btn btn-success btn-lg">Yes</a>
-                            <button class="btn btn-default btn-lg mb-control-close">No</button>
+                            <a href="{{URL::to("id/sign-out")}}" class="btn btn-success btn-lg">Sim</a>
+                            <button class="btn btn-default btn-lg mb-control-close">Não</button>
                         </div>
                     </div>
                 </div>
